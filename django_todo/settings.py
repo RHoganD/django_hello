150ac9d7a2d100af94e55db9ddecfb0e6e4ec81c
RHoganD <richpatric@hotmail.com>
2023-05-27 22:25:35 +0000

    ready to deploy

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,13 +79,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+     os.environ.get("DATABASE_URL")
+     #'default': dj_database_url.parse('postgres://empxomos:O_bemOc24LiXsXva_fSJlEMHwHcOUCgp@trumpet.db.elephantsql.com/empxomos')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
