9912628f1403b3768e500218354052c044de2c91
RHoganD <richpatric@hotmail.com>
2023-05-28 09:20:57 +0000

    fixing after deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-ia(f7r#(8=))6#-pds0bwwmrl&j9r3=*+z^$z+kte^k@iyv^(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django1-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['todo5-app.herokuapp.com']
 
 
 # Application definition
